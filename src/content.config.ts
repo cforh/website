fd0e64bc1e9a847d2de7efb652658291ba32273c
Robin <git+r@obin.ch>
2025-07-26 16:56:11 +0000

    feat: add navigation

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,26 @@
-import { extendI18nLoaderSchema, i18nLoader } from "astro-loader-i18n";
+import { extendI18nLoaderSchema, i18nContentLoader, i18nLoader, localized as localizedSchema } from "astro-loader-i18n";
 import { defineCollection, z } from "astro:content";
+import { localeSlugs } from "./site.config";
 
+const localized = <T extends z.ZodTypeAny>(schema: T) => localizedSchema(schema, localeSlugs);
+
+const navigationCollection = defineCollection({
+  loader: i18nContentLoader({ pattern: "**/[^_]*.yml", base: "./src/content/navigation" }),
+  schema: ({ image }) =>
+    extendI18nLoaderSchema(
+      z.object({
+        items: localized(
+          z.array(
+            z.object({
+              title: z.string(),
+              path: z.string().url().or(z.string()),
+              icon: image().optional(),
+            }),
+          ),
+        ),
+      }),
+    ),
+});
 const pagesCollection = defineCollection({
   loader: i18nLoader({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/pages" }),
   schema: extendI18nLoaderSchema(
@@ -13,5 +33,6 @@ const pagesCollection = defineCollection({
 });
 
 export const collections = {
+  navigation: navigationCollection,
   pages: pagesCollection,
 };
--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -1,5 +1,8 @@
 import { createI18nCollection, i18nPropsAndParams, resolvePath as originalResolvePath } from "astro-loader-i18n";
 import { C, localeSlugs } from "../site.config";
+import { getEntry, type CollectionKey, type DataEntryMap } from "astro:content";
+
+const PROTOCOL_DELIMITER = "://";
 
 export const defaultPropsAndParamsOptions = {
   defaultLocale: C.DEFAULT_LOCALE,
@@ -18,4 +21,43 @@ export const generateGetStaticIndexPaths = (routePattern: string) => {
       routePattern,
     });
   };
+};
+
+export const convertReferenceToPath = async (path: string) => {
+  if (!path.includes(PROTOCOL_DELIMITER)) return path;
+  if (path.includes(PROTOCOL_DELIMITER) && path.startsWith("http")) return path;
+
+  const [collection, reference] = path.split(PROTOCOL_DELIMITER);
+  if (!collection || !reference) throw new Error("Invalid path");
+
+  const collectionName = collection as CollectionKey;
+  const entry = await getEntry(collectionName, reference);
+
+  if (!entry) throw new Error(`Entry not found: ${path}`);
+  if (!("locale" in entry.data)) throw new Error("Entry has no locale");
+  if (!("path" in entry.data)) throw new Error("Entry has no path");
+  if (typeof entry.data.path !== "string") throw new Error("Entry title is not a string");
+
+  const localeSlug = entry.data.locale === C.DEFAULT_LOCALE ? undefined : entry.data.locale;
+
+  return originalResolvePath(localeSlug, entry.data.contentPath, entry.data.path);
+};
+
+export const prepareNavigation = async <E extends keyof DataEntryMap["navigation"] | (string & {})>(id: E) => {
+  const navigation = await getEntry("navigation", id);
+
+  if (!navigation) throw new Error(`Navigation entry not found`);
+
+  return Array.isArray(navigation.data.items)
+    ? await Promise.all(
+        navigation.data.items.map(async (item) => {
+          const { title, path, icon } = item;
+          return {
+            title,
+            icon,
+            path: await convertReferenceToPath(path),
+          };
+        }),
+      )
+    : [];
 };
