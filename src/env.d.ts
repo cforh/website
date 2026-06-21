a71b4e68d5c702d4b4832e111327376aef8c05fe
Robin <git+r@obin.ch>
2026-06-21 14:42:35 +0000

    chore: clean up cards
    see #130

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,2 @@
 declare module "@fontsource-variable/nunito";
 declare module "@fontsource-variable/montserrat";
-
-export type ImageAsset = {
-  image: ImageMetadata;
-  alt: string;
-};
---
+++ src/types.d.ts
@@ -0,0 +1,6 @@
+export type ImageAsset = {
+  image: ImageMetadata;
+  alt: string;
+};
+
+export type TitleLevels = "h2" | "h3" | "h4" | "h5" | "h6";
